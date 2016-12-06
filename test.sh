e973443bc3e1d9958cb80ee6160127264c59aa9b
kokan <kokaipeter@gmail.com>
2016-12-06 22:13:59 +0000

    arch, ubuntu, centos support, remove unused

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+docker run        \
+ -it              \
+ -v $(pwd):/thenv \
+ dock0/arch       \
+ bash -c "/thenv/thenv && /thenv/thenv"
+
+docker run        \
+ -it              \
+ -v $(pwd):/thenv \
+ ubuntu:16.10     \
+ bash -c "apt update && /thenv/thenv && /thenv/thenv"
+
+docker run        \
+ -it              \
+ -v $(pwd):/thenv \
+ centos:latest    \
+ bash -c "yum install -y epel-release && /thenv/thenv && /thenv/thenv"
+
